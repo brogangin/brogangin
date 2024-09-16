8398f0ae09f46df02f8b7f9eb5446fe6bc24e29b
brogangin <terompettrumpet@gmail.com>
2024-09-16 21:41:16 +0700

    fix layout and minor details

--- src/app/components/projectSection.tsx
+++ src/app/components/projectSection.tsx
@@ -10,7 +10,7 @@ const projectsData = [
     title: "React Portfolio Website",
     description: "My Own Portfolio Website build with NextJs",
     image: "/images/projects/1.jpg",
-    tag: ["All", "Web"],
+    // tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -19,7 +19,7 @@ const projectsData = [
     title: "Soccer Score Website",
     description: "Displaying soccer fixtures and results from top 5 leagues in europe",
     image: "/images/projects/2.jpg",
-    tag: ["All", "Web"],
+    // tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/soccer",
   },
@@ -28,7 +28,7 @@ const projectsData = [
     title: "SiPerpus",
     description: "Book Lending Information System built using CodeIgniter4",
     image: "/images/projects/3.jpg",
-    tag: ["All", "Web"],
+    // tag: ["All", "Web"],
     gitUrl: "//github.com/brogangin/SiPerpus-CodeIgniter4",
     previewUrl: "/",
   },
@@ -36,8 +36,8 @@ const projectsData = [
     id: 4,
     title: "Food Ordering Application",
     description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
+    image: "/images/projects/project.png",
+    // tag: ["All", "Mobile"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -45,32 +45,32 @@ const projectsData = [
     id: 5,
     title: "React Firebase Template",
     description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
+    image: "/images/projects/project.png",
+    // tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id: 6,
     title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
+    description: "Project 6 description",
+    image: "/images/projects/project.png",
+    // tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
-  const [tag, setTag] = useState("All");
+  // const [tag, setTag] = useState("All");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-  const handleTagChange = (newTag: string) => {
-    setTag(newTag);
-  };
+  // const handleTagChange = (newTag: string) => {
+  //   setTag(newTag);
+  // };
 
-  const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
+  // const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
 
   const cardVariants = {
     initial: { y: 50, opacity: 0 },
@@ -78,20 +78,22 @@ const ProjectsSection = () => {
   };
 
   return (
-    <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+    <section className="py-8" id="projects">
+      <div className=" py-8 px-4 sm:py-16 xl:px-16">
+        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
+        {/* <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
         <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
         <ProjectTag onClick={handleTagChange} name="Mobile" isSelected={tag === "Mobile"} />
+      </div> */}
+        <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+          {projectsData.map((project, index) => (
+            <motion.li key={index} variants={cardVariants} initial="initial" animate={isInView ? "animate" : "initial"} transition={{ duration: 0.6, delay: index * 0.4 }}>
+              <ProjectCard project={project} />
+            </motion.li>
+          ))}
+        </ul>
       </div>
-      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project, index) => (
-          <motion.li key={index} variants={cardVariants} initial="initial" animate={isInView ? "animate" : "initial"} transition={{ duration: 0.3, delay: index * 0.4 }}>
-            <ProjectCard project={project} />
-          </motion.li>
-        ))}
-      </ul>
     </section>
   );
 };
