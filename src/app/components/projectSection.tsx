b649b7b46766295d8cf3986b40f47f27cd12126b
brogangin <terompettrumpet@gmail.com>
2025-02-24 14:13:33 +0700

    update projects

--- src/app/components/projectSection.tsx
+++ src/app/components/projectSection.tsx
@@ -5,97 +5,107 @@ import ProjectTag from "./projectTag";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
-  {
-    id: 1,
-    title: "React Portfolio Website",
-    description: "My Own Portfolio Website build with NextJs",
-    image: "/images/projects/1.jpg",
-    // tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 2,
-    title: "Soccer Score Website",
-    description: "Displaying soccer fixtures and results from top 5 leagues in europe",
-    image: "/images/projects/2.jpg",
-    // tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/soccer",
-  },
-  {
-    id: 3,
-    title: "SiPerpus",
-    description: "Book Lending Information System built using CodeIgniter4",
-    image: "/images/projects/3.jpg",
-    // tag: ["All", "Web"],
-    gitUrl: "//github.com/brogangin/SiPerpus-CodeIgniter4",
-    previewUrl: "/",
-  },
-  {
-    id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
-    image: "/images/projects/project.png",
-    // tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
-    image: "/images/projects/project.png",
-    // tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 6 description",
-    image: "/images/projects/project.png",
-    // tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
+    {
+        id: 1,
+        title: "React Portfolio Website",
+        description: "My Own Portfolio Website build with NextJs",
+        image: "/images/projects/1.jpg",
+        // tag: ["All", "Web"],
+        gitUrl: "//github.com/brogangin/brogangin",
+        previewUrl: "/",
+    },
+    {
+        id: 2,
+        title: "Soccer Score Website",
+        description: "Displaying soccer fixtures and results from top 5 leagues in europe",
+        image: "/images/projects/2.jpg",
+        // tag: ["All", "Web"],
+        gitUrl: "//github.com/brogangin/brogangin/tree/master/src/app/soccer",
+        previewUrl: "/soccer",
+    },
+    {
+        id: 3,
+        title: "SiPerpus",
+        description: "Book Lending Information System built using CodeIgniter4",
+        image: "/images/projects/3.jpg",
+        // tag: ["All", "Web"],
+        gitUrl: "//github.com/brogangin/SiPerpus-CodeIgniter4",
+        previewUrl: "#",
+    },
+    {
+        id: 4,
+        title: "Chatbot Village's Information",
+        description:
+            "Village information service chatbot that has the ability to explain information about Sawotratap Village",
+        image: "/images/projects/4.png",
+        // tag: ["All", "Mobile"],
+        gitUrl: "//github.com/brogangin/sawotratap.chat-bot",
+        previewUrl: "//sawotratap-chat-bot.vercel.app/",
+    },
+    {
+        id: 5,
+        title: "TimelessTales",
+        description:
+            "A website that gives you a personalized page to showcase precious moments in your life",
+        image: "/images/projects/5.png",
+        // tag: ["All", "Web"],
+        gitUrl: "//github.com/brogangin/CilukBa_WebDesign",
+        previewUrl: "//cilukba.vercel.app/",
+    },
+    {
+        id: 6,
+        title: "15Libels",
+        description: "A simple website to show my design taste",
+        image: "/images/projects/6.png",
+        // tag: ["All", "Web"],
+        gitUrl: "//github.com/brogangin/15libels",
+        previewUrl: "//brogangin.github.io/15libels/",
+    },
 ];
 
 const ProjectsSection = () => {
-  // const [tag, setTag] = useState("All");
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+    // const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
-  // const handleTagChange = (newTag: string) => {
-  //   setTag(newTag);
-  // };
+    // const handleTagChange = (newTag: string) => {
+    //   setTag(newTag);
+    // };
 
-  // const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
+    // const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
 
-  const cardVariants = {
-    initial: { y: 50, opacity: 0 },
-    animate: { y: 0, opacity: 1 },
-  };
+    const cardVariants = {
+        initial: { y: 50, opacity: 0 },
+        animate: { y: 0, opacity: 1 },
+    };
 
-  return (
-    <section className="py-8" id="projects">
-      <div className=" py-8 px-4 sm:py-16 xl:px-16">
-        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
-        {/* <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+    return (
+        <section className="py-8" id="projects">
+            <div className=" py-8 px-4 sm:py-16 xl:px-16">
+                <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+                    My Projects
+                </h2>
+                {/* <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
         <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
         <ProjectTag onClick={handleTagChange} name="Mobile" isSelected={tag === "Mobile"} />
       </div> */}
-        <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-          {projectsData.map((project, index) => (
-            <motion.li key={index} variants={cardVariants} initial="initial" animate={isInView ? "animate" : "initial"} transition={{ duration: 0.6, delay: index * 0.4 }}>
-              <ProjectCard project={project} />
-            </motion.li>
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
+                <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+                    {projectsData.map((project, index) => (
+                        <motion.li
+                            key={index}
+                            variants={cardVariants}
+                            initial="initial"
+                            animate={isInView ? "animate" : "initial"}
+                            transition={{ duration: 0.6, delay: index * 0.4 }}
+                        >
+                            <ProjectCard project={project} />
+                        </motion.li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
 };
 
 export default ProjectsSection;
